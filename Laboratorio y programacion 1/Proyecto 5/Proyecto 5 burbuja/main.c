d771073af739c80df0e23ee50a8ade627e8ebfd5
Lucas3110 <47362417+Lucas3110@users.noreply.github.com>
2019-04-04 21:54:31 -0300

    funcion getArrayInt + funcion swap burbujeo

---
+++ Laboratorio y programacion 1/Proyecto 5/Proyecto 5 burbuja/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+//forma menos ineficiente sin usar los 2 for
+// quicksort para ordenar kilombo, inserction para insertar algo perdido
+int main()
+{
+limite = 4;
+
+do
+{
+for(i=0,i<limite-i;i++)
+{   flagswap=0;
+    if(a[i]>a[i+1])
+    {
+        swap(a[i];a[i+1])
+        flagswap=1;
+    }
+}
+
+    return 0;
+}
+
+}while(flagswap) //se termina si queda en 0 porque es falso
+//swap no existe, hay que hacerla en funcion
+
+
+int swap(A[i],A[i+1])
+{
+  int buffer =0;
+
+  aux = A[i];
+  A[i] = A[i+1];
+  A[i+1] = aux;
+
+}
