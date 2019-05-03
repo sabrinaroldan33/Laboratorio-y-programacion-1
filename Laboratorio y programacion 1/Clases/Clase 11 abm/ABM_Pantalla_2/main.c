75cbaf584e5ac0acc02ba80e688bdb558392bb1c
Lucas3110 <47362417+Lucas3110@users.noreply.github.com>
2019-05-02 21:52:49 -0300

    Clase 11 abm + 2 informes incompletos

---
+++ Laboratorio y programacion 1/Clases/Clase 11 abm/ABM_Pantalla_2/main.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utn_strings.h"
+#include "Pantalla.h"
+#include "Publicidad.h"
+#define REINTENTOS 3
+#define LEN_LISTA 5
+
+void ordenarPantallaPrecio(Pantalla *pPantalla,int limite);
+
+
+int main()
+{
+    int flag=0;
+    int posLibre;
+    int opcion=0;
+    Pantalla visores[LEN_LISTA];
+    Publicidad Marketing[LEN_LISTA];
+
+
+    pan_initPantalla(visores,LEN_LISTA);
+    pub_initPublicidad(Marketing,LEN_LISTA);
+
+    //******************* SACAR *****************
+    strcpy(visores[0].direccion,"Mitre 750");
+    visores[0].isEmpty=0;
+    strcpy(visores[0].nombre,"Pantalla 1");
+    visores[0].precio= 1000.20;
+    visores[0].tipo=0;
+    visores[0].idPantalla=0;
+
+    strcpy(visores[1].direccion,"Belgrano 750");
+    visores[1].isEmpty=0;
+    strcpy(visores[1].nombre,"Pantalla 2");
+    visores[1].precio= 2000.20;
+    visores[1].tipo=0;
+    visores[1].idPantalla=1;
+
+    flag=1;
+
+    strcpy(Marketing[0].archivo,"archivo1.mp4");
+    strcpy(Marketing[0].cuit,"20-20056658-5");
+    Marketing[0].dias = 30;
+    Marketing[0].isEmpty = 0;
+    Marketing[0].idPublicidad = 0;
+    Marketing[0].idPantalla = 0;
+
+    strcpy(Marketing[1].archivo,"archivo2.mp4");
+    strcpy(Marketing[1].cuit,"20-20056658-5");
+    Marketing[1].dias = 30;
+    Marketing[1].isEmpty = 0;
+    Marketing[1].idPublicidad = 1;
+    Marketing[1].idPantalla = 0;
+
+    strcpy(Marketing[2].archivo,"archivo3.mp4");
+    strcpy(Marketing[2].cuit,"20-20056659-5");
+    Marketing[2].dias = 30;
+    Marketing[2].isEmpty = 0;
+    Marketing[2].idPublicidad = 2;
+    Marketing[2].idPantalla = 1;
+    //******************* SACAR *****************
+
+
+    while(opcion!=6)
+    {
+        getIntInRange(&opcion,"\n1)Alta de pantalla\n2)Modificar datos de pantalla\n3)Baja de pantalla\n"
+                      "4)Contratar Publicidad\n5)Mostrar Pantallas\n6)Salir\n7)Ordenar Pantallas por Precio\n\n    INGRESE OPCION: ","ERROR\n",1,8,3);
+        switch(opcion)
+        {
+            case 1:
+            {
+                posLibre=pan_findFree(visores,LEN_LISTA);
+                if(posLibre>=0)
+                {
+                    printf("\n----Se encontro lugar----\n");
+                    if(!pan_addPan(visores,LEN_LISTA,posLibre,"DATO NO VALIDO\n",REINTENTOS))
+                    {
+                        flag=1;
+                        printf("\n----Se dio de ALTA exitosamente!----\n");
+                    }
+                    else
+                    {
+                        printf("\n----No se realizo el ALTA!----\n");
+                    }
+                }
+                else
+                {
+                    printf("\n----No se encontro lugar libre!----\n");
+                }
+                break;
+            }
+            case 2:
+            {
+                if(flag)
+                {
+                    if(pan_alter(visores,LEN_LISTA,"DATO NO VALIDO\n",REINTENTOS))
+                    {
+                        printf("\n----Se modifico exitosamente----\n");
+                    }
+                }
+                else
+                {
+                    printf("\n----No hay ALTAS en la NOMINA!----\n");
+                }
+                break;
+            }
+            case 3:
+            {
+                if(flag)
+                {
+                    if(pan_removePantalla(visores,LEN_LISTA,"DATO NO VALIDO\n",REINTENTOS))
+                    {
+                        printf("\n----La BAJA se realizo con exito!----\n");
+                    }
+                }
+                else
+                {
+                    printf("\n----No hay ALTAS en la NOMINA!----\n");
+                }
+                break;
+            }
+            case 4:
+            {
+                if(flag)
+                {
+                    pan_printPantalla(visores,LEN_LISTA);
+                    if(!pub_addPub(Marketing,visores,LEN_LISTA,"\nDATO NO VALIDO\n",REINTENTOS))
+                    {
+                        printf("\n----La CONTRATACION se realizo con exito!----\n");
+                    }
+                    else
+                    {
+                        printf("\n----No pudo CONTRATARSE la pantalla!----\n");
+                    }
+                }
+                else
+                {
+                    printf("\n----No hay PANTALLAS en la NOMINA!----\n");
+                }
+                break;
+            }
+            case 5:
+            {
+                if(flag)
+                {
+                    pan_printPantalla(visores,LEN_LISTA);
+                }
+                break;
+            }
+            case 7: //informes
+            {
+
+                ordenarPantallaPrecio(visores,1);
+
+
+                //No copie el while bien del pibe que lo hizo
+
+
+            }
+            case 8:
+            {
+
+
+
+            }
+        }
+    }
+    return 0;
+}
+
+//FUNCIONES INFORMES:
+
+void ordenarPantallaPrecio(Pantalla *pPantalla,int limite)
+{
+
+int j;
+int flag=0;
+Pantalla buffer;
+do
+{
+    flag=0;
+    for(j=0;j<limite-1;j++) //no comparar elementos que estan vacios y forzar la proxima iteracion
+    {
+        if(pPantalla[j].precio<pPantalla[j+1].precio)
+        {
+            buffer=pPantalla[j];
+            pPantalla[j]=pPantalla[j+1];
+            pPantalla[j+1]=buffer;
+            flag=1;
+        }
+        else if(pPantalla[j].precio==pPantalla[j+1].precio)//si los precios son iguales ordeno por nombrea abajo
+        {
+            if(strncmp(pPantalla[j].nombre,pPantalla[j+1].nombre,50)>0) //ordeno por nombre si los precios son iguales
+            {
+                buffer=pPantalla[j];
+                pPantalla[j]=pPantalla[j+1];
+                pPantalla[j+1]=buffer;
+                flag=1;
+            }
+        }
+    }
+
+  }while(flag);
+
+}
+
+
+void ordenarContrataciones()
+
